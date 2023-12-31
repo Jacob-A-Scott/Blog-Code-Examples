8cfc5cb358ac7fba2492843c4f5b71f99ad46208
jacob <jacobscott8910@gmail.com>
2023-12-31 13:32:26 -0500

    tweaks to gene_test_simulation.py

--- Bayes Theorem/gene_test_simulation.py
+++ Bayes Theorem/gene_test_simulation.py
@@ -1,37 +1,30 @@
-# %%
+# %% Imports
 import numpy as np
 import seaborn as sns
 
-# %%
-# Parameters for the scenario
+# %% Setting variables for scenario parameters
 population = 1000000  # size of the population to simulate
-prevalence = 0.02  # 2% of the population has the disease
+prevalence = 0.02  # 2% of the population has the gene
 sensitivity = 0.95  # true positive rate
 specificity = 0.90  # true negative rate
 
-# Simulating the population
-# True for individuals with the disease, False for those without
-disease = np.random.rand(population) < prevalence
+# %% Simulating the population
+# True for individuals with the gene, False for those without
+gene = np.random.rand(population) < prevalence
 
-# Simulating test results
+# %% Simulating test results
 # True for a positive test, False for a negative test
-positive_test_given_disease = np.random.rand(population) < sensitivity
-negative_test_given_no_disease = np.random.rand(population) < specificity
+positive_test_given_gene = np.random.rand(population) < sensitivity
+negative_test_given_no_gene = np.random.rand(population) < specificity
 
-# True positive: disease and positive test
-# False positive: no disease but positive test
-positive_test = (disease & positive_test_given_disease) | (~disease & ~negative_test_given_no_disease)
+# True positive: gene and positive test
+# False positive: no gene but positive test
+positive_test = (gene & positive_test_given_gene) | (
+    ~gene & ~negative_test_given_no_gene
+)
 
-# Calculating the probability of having the disease given a positive test result
-probability_disease_given_positive_test = np.mean(disease[positive_test])
+# %% Final probability
+# Calculating the probability of having the gene given a positive test result
+probability_gene_given_positive_test = np.mean(gene[positive_test])
 
-probability_disease_given_positive_test
-
-# %%
-unique, counts = np.unique(disease[positive_test], return_counts=True)
-print(unique, counts)
-
-# %%
-
-
-
+print(f"Probability of having the gene, given a positive test: {probability_gene_given_positive_test}")
